35cd688e5396ef3b6169d8725a3eb87d0edb0158
Pathum Danthanarayana <61627702+pathumd@users.noreply.github.com>
2021-06-10 18:48:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+feather.replace()
+// Main h1 animation
+var textWrapper = document.querySelector('.mainh1');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.mainh1 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 2250,
+    delay: (el, i) => 150 * (i+1)
+  }).add({
+    targets: '.mainh1',
+    opacity: 100,
+  });
+
